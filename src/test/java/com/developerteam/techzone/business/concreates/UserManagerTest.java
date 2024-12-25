c2255f78cbc192bb04c6f6713cbc13409fa08fdd
Nursena Zengin <139920373+nursenazengin@users.noreply.github.com>
2024-12-26 00:00:53 +0300

    Update UserManagerTest.java

--- src/test/java/com/developerteam/techzone/business/concreates/UserManagerTest.java
+++ src/test/java/com/developerteam/techzone/business/concreates/UserManagerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,19 +16,16 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserManagerTest {
 
+    @Autowired
     private UserManager userManager;
 
     @Autowired
     private IUserRepository userRepository;
 
-    @BeforeEach
-    void setUp() {
-        userManager = new UserManager(userRepository);
-    }
 
     @Test
     void testGetAll() {
