fe6d9ca7a21568be388894f0b9f09bb86c440cae
Nursena Zengin <139920373+nursenazengin@users.noreply.github.com>
2024-12-26 12:02:09 +0300

    Update CategoryManagerTest.java

--- src/test/java/com/developerteam/techzone/business/concreates/CategoryManagerTest.java
+++ src/test/java/com/developerteam/techzone/business/concreates/CategoryManagerTest.java
@@ -1,6 +1,7 @@
 package com.developerteam.techzone.business.concreates;
 
 import com.developerteam.techzone.dataAccess.abstracts.ICategoryRepository;
+import com.developerteam.techzone.entities.concreates.Brand;
 import com.developerteam.techzone.entities.concreates.Category;
 import com.developerteam.techzone.entities.dto.DtoCategory;
 import com.developerteam.techzone.entities.dto.DtoCategoryIU;
@@ -92,6 +93,15 @@ class CategoryManagerTest {
         Category category = categoryRepository.getById(3);
         assertNotNull(category);
         categoryRepository.delete(category);
+    }
+
+    @Test
+    void testFindCategoryOrThrow() {
+        Category result = categoryManager.findCategoryOrThrow(1);
+
+        assertNotNull(result);
+        assertEquals(1, result.getId(), "The category ID should match.");
+        assertEquals("Bilgisayar", result.getName());
     }
 }
 
