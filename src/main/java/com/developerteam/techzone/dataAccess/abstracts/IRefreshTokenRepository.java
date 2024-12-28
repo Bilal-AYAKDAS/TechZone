72b2e7f2e57fa97b70318f70919e3a5adc207a7f
Bilal Ayakdaş <bilalayakdas@gmail.com>
2024-12-28 23:42:19 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/developerteam/techzone/dataAccess/abstracts/IRefreshTokenRepository.java
+++ src/main/java/com/developerteam/techzone/dataAccess/abstracts/IRefreshTokenRepository.java
@@ -1,13 +1,28 @@
 package com.developerteam.techzone.dataAccess.abstracts;
 
 import com.developerteam.techzone.entities.concreates.RefreshToken;
+import com.developerteam.techzone.entities.concreates.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IRefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
 
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+    List<RefreshToken> findByUser(User user);
+    
+    @Transactional
+    void deleteByUser(User user);
+
+
+
+
+
+
 
 }
