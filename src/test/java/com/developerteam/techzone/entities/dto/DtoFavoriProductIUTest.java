e0dd82c6dc4a8f6f284a8a422a29ccfcb65abedd
Nursena Zengin <139920373+nursenazengin@users.noreply.github.com>
2024-12-31 10:49:27 +0300

    Update DtoFavoriProductIUTest.java

--- src/test/java/com/developerteam/techzone/entities/dto/DtoFavoriProductIUTest.java
+++ src/test/java/com/developerteam/techzone/entities/dto/DtoFavoriProductIUTest.java
@@ -10,63 +10,24 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DtoProductIUTest {
+class DtoFavoriProductIUTest {
 
     private final Validator validator;
 
-    public DtoProductIUTest() {
+    public DtoFavoriProductIUTest() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         this.validator = factory.getValidator();
     }
 
     @Test
-    void testValidDtoProductIU() {
-        DtoProductIU dto = new DtoProductIU();
-        dto.setName("Valid Product");
-        dto.setPrice(100.0);
-        dto.setStockAmount(10);
-        dto.setDescription("This is a valid description.");
-        dto.setCategoryId(1);
-        dto.setBrandId(1);
+    void testValidProductId() {
+        DtoFavoriProductIU dto = new DtoFavoriProductIU();
+        dto.setProductId(1);
 
+        Set<ConstraintViolation<DtoFavoriProductIU>> violations = validator.validate(dto);
 
-        Set<ConstraintViolation<DtoProductIU>> violations = validator.validate(dto);
-
-        assertTrue(violations.isEmpty(), "Valid DTO should not cause validation errors.");
+        assertTrue(violations.isEmpty());
     }
 
-    @Test
-    void testInvalidNameTooShort() {
-        DtoProductIU dto = new DtoProductIU();
-        dto.setName("A"); // Çok kısa
-        dto.setPrice(100.0);
-        dto.setStockAmount(10);
-        dto.setDescription("This is a valid description.");
-        dto.setCategoryId(1);
-        dto.setBrandId(1);
 
-
-        Set<ConstraintViolation<DtoProductIU>> violations = validator.validate(dto);
-
-        assertFalse(violations.isEmpty(), "Name too short should cause validation errors.");
-        ConstraintViolation<DtoProductIU> violation = violations.iterator().next();
-        assertEquals("Product name must be between 2 and 30 characters.", violation.getMessage());
-    }
-
-    @Test
-    void testInvalidDescriptionTooLong() {
-        DtoProductIU dto = new DtoProductIU();
-        dto.setName("Valid Product");
-        dto.setPrice(100.0);
-        dto.setStockAmount(10);
-        dto.setDescription("A".repeat(501)); // Çok uzun açıklama
-        dto.setCategoryId(1);
-        dto.setBrandId(1);
-
-        Set<ConstraintViolation<DtoProductIU>> violations = validator.validate(dto);
-
-        assertFalse(violations.isEmpty(), "Description too long should cause validation errors.");
-        ConstraintViolation<DtoProductIU> violation = violations.iterator().next();
-        assertEquals("Description  must be small  500 characters.", violation.getMessage());
-    }
 }
