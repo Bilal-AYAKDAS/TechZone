7313165c665cbab8e59c299fd2797e5fe77705e6
Bilal Ayakdaş <bilalayakdas@gmail.com>
2025-01-03 20:36:38 +0300

    Added project report and postman collection

--- src/main/java/com/developerteam/techzone/config/WebConfig.java
+++ src/main/java/com/developerteam/techzone/config/WebConfig.java
@@ -1,17 +1,38 @@
 package com.developerteam.techzone.config;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
+import java.io.File;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir; // application.properties'ten alınan yükleme yolu
+
+    @PostConstruct
+    public void init() {
+        // Klasör yoksa oluştur
+        File directory = new File(uploadDir);
+        if (!directory.exists()) {
+            boolean created = directory.mkdirs();
+            if (created) {
+                System.out.println("Uploads klasörü başarıyla oluşturuldu: " + directory.getAbsolutePath());
+            } else {
+                System.err.println("Uploads klasörü oluşturulamadı.");
+            }
+        }
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Ana dizindeki 'uploads' klasörünü URL ile ilişkilendir
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations("file:"+uploadDir);
     }
 
 }
